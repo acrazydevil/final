cb372a1e65b00a42ba1345e520611c8888e80024
LIN,Kuo-Yuan （林郭芫） <117023861+acrazydevil@users.noreply.github.com>
2024-11-09 12:32:54 +0800

    Add files via upload

--- voice_pro_max/c.py
+++ voice_pro_max/c.py
@@ -2,13 +2,93 @@ from flask import Flask, render_template, jsonify,request
 import speech_recognition as sr
 from ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger
 import soundfile
-import subprocess
+import time
+import json
+from paho.mqtt import client as mqtt
+import roslibpy
+
 app = Flask(__name__,static_url_path='/static')
+
+client_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+mqttClient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1,client_id)
+
+# IP & Port of Thouzer Basic
+ip = '192.168.212.1'
+port = 1883
+
+# Username & Password of Thouzer Basic
+username = 'SmartRobot'
+password = 'SmartRobot'
+
+topic_pub = '0/THOUZER_HW/RMS-10B1-AAJ65/exec/cmd'
+address_api = '/home/wen/Desktop/voice_pro_max/mqtt_api'
+topic_sub='0/THOUZER_HW/RMS-10B1-AAJ65/event/app'
+
+
+def mqtt_connect():
+        mqttClient.username_pw_set(username, password)  # MQTT Server PW
+        mqttClient.on_connect = mqtt_on_connect
+        mqttClient.on_message = on_message  
+        mqttClient.connect(ip, port, 60)
+        mqttClient.loop_start()
+        # print('Connect Sucessfully')
+
+def start_motion_json():
+        with open(address_api + '/start_motion.json', 'r') as read_file :
+            api = json.load(read_file)
+        return api
+
+def open_json(n):
+        if n == 1 :
+            with open(address_api + '/MT_MemorizeMode.json', 'r') as read_file :
+                api = json.load(read_file)
+        elif n == 2 :
+            with open(address_api + '/pub_MT_700.json', 'r') as read_file :
+                api = json.load(read_file)
+        elif n == 3:
+            with open(address_api + '/pub_MT_701.json', 'r') as read_file :
+                api = json.load(read_file)
+        return api
+
+def on_publish_start():
+        msg = str(start_motion_json())
+        msg = msg.replace("'",'"')
+        # print(msg)
+        mqttClient.publish(topic_pub, f'{msg}')
+
+def on_publish(n):
+        msg = str(open_json(n))
+        msg = msg.replace("'",'"')
+        # print(msg)
+        mqttClient.publish(topic_pub, f'{msg}')
+
+
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload.decode())
+    status=payload['data']['status']
+    if payload['data']['status']=='success':
+        listener.publish(roslibpy.Message({'data':status}))
+    # print("[{}]: {}".format(msg.topic, str(msg.payload)))
+
+def mqtt_on_connect(client, userdata, flags, rc) :
+    if rc == 0 :
+        # print('Connect to MQTT Broker')
+        mqttClient.subscribe(topic_sub)
+    else :
+        print('Connect failed')
+    
+def callback(message):
+    mqtt_connect()
+    on_publish_start()
+    time.sleep(1.5)
+    on_publish(3)
+    # print('{}'.format(message['data']))
+    # print(555)
 
 # 路由函数，用于执行语音识别和关键字提取
 @app.route('/execute_speech_recognition', methods=['POST'])
 def execute_speech_recognition():
-    
+    global n
     audio_file = request.files['audio']
     data, samplerate = soundfile.read(audio_file)
 
@@ -52,7 +132,6 @@ def execute_speech_recognition():
         # 提取名詞和動詞作為關鍵字
         place = extract_keywords(sentence_ws, sentence_pos, ['Nc'])
         things = extract_keywords(sentence_ws, sentence_pos, ['Na'])
-        number = extract_keywords(sentence_ws, sentence_pos, ['Neu'])
     print(place,things)
     result = {
             'text': text,
@@ -60,25 +139,33 @@ def execute_speech_recognition():
             'things':things
     }
 
-    return jsonify(result)
-def docker_compose_up():
-    # 定義命令和參數
-    command = ['docker', 'compose', 'up', 'robot']
-    # 執行命令
-    subprocess.run(command)
-    # 檢查命令是否成功執行
-    # if result.returncode == 0:
-    #     print("Command executed successfully!")
-    #     print("Output:", result.stdout)
-    # else:
-    #     print("Command failed.")
-    #     print("Error:", result.stderr)
+    #===========================================================================
 
-docker_compose_up()
+    mqtt_connect()
+    on_publish_start()
+    time.sleep(1.5)
+    if text[0:4]=='路線規劃':
+        on_publish(1)
+    elif place[0]=='實驗室':
+        on_publish(2)
+    elif place[0]=='辦公室':
+        on_publish(3)
+    # on_subscribe()
+    return jsonify(result)
+
 # 主页路由
 @app.route('/')
 def index():
     return render_template('index1.html')
 
 if __name__ == '__main__':
+    client = roslibpy.Ros(host='localhost', port=9090)
+    client.run()
+    listener=roslibpy.Topic(client,'/amr_arm_com','/std_msgs/String')
+    listener2=roslibpy.Topic(client,'/amr_arm_com2','/std_msgs/String')
+    listener2.subscribe(callback)
     app.run(debug=True, port=5000)
+
+#ngrok http --domain=hip-roughly-falcon.ngrok-free.app localhost:5000
+#roslaunch rosbridge_server rosbridge_websocket.launch
+#rosrun tf2_web_republisher tf2_web_republisher
